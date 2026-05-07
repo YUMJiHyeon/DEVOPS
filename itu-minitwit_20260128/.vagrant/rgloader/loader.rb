ce73250481ac860e251b60c2fe8cc25ab4494d8b
Magga21 <margretf24@ru.is>
2026-05-07 13:29:21 +0200

    Fixed itu-minitwit_20260128/.vagrant/rgloader/loader.rb so SonarCude stops complaining

--- itu-minitwit_20260128/.vagrant/rgloader/loader.rb
+++ itu-minitwit_20260128/.vagrant/rgloader/loader.rb
@@ -3,10 +3,9 @@
 
 # This file loads the proper rgloader/loader.rb file that comes packaged
 # with Vagrant so that encoded files can properly run with Vagrant.
-
+require File.expand_path(
+    "rgloader/loader", ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"]) 
 if ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"]
-  require File.expand_path(
-    "rgloader/loader", ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"])
 else
-  raise "Encoded files can't be read outside of the Vagrant installer."
+  raise RuntimeError, "Encoded files can't be read outside of the Vagrant installer."
 end
